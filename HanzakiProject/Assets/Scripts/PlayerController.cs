3256c13eebe33d151b66ac87b503a1114efcd512
happy30 <jordivdh@hotmail.com>
2016-09-07 15:05:26 +0200

    added ignorefile

--- HanzakiProject/Assets/Scripts/PlayerController.cs
+++ HanzakiProject/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,50 @@
-﻿using UnityEngine;
+﻿// PlayerController by Jordi
+
+using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    //Components
+    Rigidbody _rb;
+    public StatsManager stats;
 
-	// Use this for initialization
-	void Start () {
-	
+    //Movement
+    float speed;
+    public float walkSpeed;
+    public float runSpeed;
+    public float jumpHeight;
+
+
+	void Start ()
+    {
+        _rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        //Change speed to runspeed if Shift is pressed
+        speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Check if player is standing on Ground
+            if (IsTouching().tag == "Ground")
+            {
+                Jump();
+            }
+        }
+    }
+
+    //Check what object is beneath the player
+    public GameObject IsTouching()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 1))
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+
 }
