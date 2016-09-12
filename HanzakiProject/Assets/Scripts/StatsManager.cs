36b80b30b98cd77a483a6f7ebd78d57acbe0bf79
happy30 <jordivdh@hotmail.com>
2016-09-12 15:31:25 +0200

    added CameraController.cs and StatsManager.cs, started with PlayerController.cs

---
+++ HanzakiProject/Assets/Scripts/StatsManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsManager : MonoBehaviour {
+
+    public float walkSpeed;
+    public float runSpeed;
+    public int health;
+    public int maxHealth;
+
+}
