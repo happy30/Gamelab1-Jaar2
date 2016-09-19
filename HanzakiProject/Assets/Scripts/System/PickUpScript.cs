232c46a625d3dad187be83fe95cdaa4ff3e31654
DESKTOP-DAPG37V\kingo <kingosman6@gmail.com>
2016-09-19 15:10:46 +0200

    Added Destroy and pickup scripts

---
+++ HanzakiProject/Assets/Scripts/System/PickUpScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpScript : MonoBehaviour {
+
+    public StatsManager stats;
+
+    public enum PickUpTypes
+    {
+        Hook,
+        Rope,
+        Heart,
+        Health,
+        Shuriken,
+        SmokeBomb,
+        Blade
+    }
+    public PickUpTypes pickUpTypes;
+
+    void Start()
+    {
+        stats = GameObject.Find("GameManager").GetComponent<StatsManager>();
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(pickUpTypes == PickUpTypes.Hook)
+            {
+                stats.AddHookPart();
+            }
+            if(pickUpTypes == PickUpTypes.Rope)
+            {
+                stats.AddHookPart();
+            }
+            if (pickUpTypes == PickUpTypes.Heart)
+            {
+                stats.GetLife();
+            }
+        }
+    }
+}
