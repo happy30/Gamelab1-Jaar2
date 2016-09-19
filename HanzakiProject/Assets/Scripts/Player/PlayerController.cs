ba094961ba50f97a9f800d40bb1c206a7a78bfb0
happy30 <jordivdh@hotmail.com>
2016-09-19 14:32:58 +0200

    Added System scripts

---
+++ HanzakiProject/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿// PlayerController by Jordi
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    //Components
+    Rigidbody _rb;
+    public StatsManager stats;
+
+    //Movement
+    float speed;
+    public float jumpHeight;
+
+    public float xMovement;
+    public float modelWidth;
+
+
+	void Start ()
+    {
+        _rb = GetComponent<Rigidbody>();
+        stats = GameObject.Find("GameManager").GetComponent<StatsManager>();
+        modelWidth = 1;
+
+    }
+	
+	void Update ()
+    {
+        //Change speed to runspeed if Shift is pressed
+        speed = Input.GetKey(KeyCode.LeftShift) ? stats.runSpeed : stats.walkSpeed;
+
+        xMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+
+        //Make the player not able to hump the wall
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.right, out hit, modelWidth))
+        {
+            if(xMovement > 0 )
+            {
+                xMovement = 0;
+            }
+        }
+        if (Physics.Raycast(transform.position, -transform.right, out hit, modelWidth))
+        {
+            if (xMovement < 0)
+            {
+                xMovement = 0;
+            }
+        }
+        transform.Translate(new Vector3(xMovement, 0, 0));
+
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Check if player is standing on Ground
+            if (IsTouching().tag == "Ground")
+            {
+                print("jump");
+            }
+        }
+    }
+
+
+    //Check what object is beneath the player
+    public GameObject IsTouching()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 1))
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+
+}
---
+++ HanzakiProject/Assets/Scripts/System/StatsManager.cs
@@ -0,0 +1,34 @@
+﻿//StatsManager by Jordi
+
+using UnityEngine;
+using System.Collections;
+
+public class StatsManager : MonoBehaviour
+{
+    public float walkSpeed;
+    public float runSpeed;
+    public int health;
+    public int maxHealth;
+
+    public bool katanaUnlocked;
+    public bool grapplingHookUnlocked;
+    public bool shurikenUnlocked;
+    public bool smokeBombUnlocked;
+
+    public int shurikenAmount;
+    public int smokeBombAmount;
+
+    public void GetLife()
+    {
+        maxHealth++;
+        health++;
+    }
+
+    public void GetHealth()
+    {
+        if(health < maxHealth)
+        {
+            health++;
+        }
+    }
+}
