b0dcfac27db20089a0ba7925191b9228601d652d
happy30 <jordivdh@hotmail.com>
2016-09-26 16:29:27 +0200

    added shuriken scripts

---
+++ HanzakiProject/Assets/Scripts/Player/Shuriken.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shuriken : MonoBehaviour {
+
+    public StatsManager stats;
+    public float attackPower;
+
+    public GameObject shurikenObject;
+    GameObject spawnedShurikenObject;
+
+
+
+    void Awake()
+    {
+        stats = GameObject.Find("GameManager").GetComponent<StatsManager>();
+    }
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKey(InputManager.Shuriken))
+        {
+            //Animatorplay blabla
+        }
+	}
+
+    public void ThrowShuriken(float attackPower)
+    {
+        Destroy(spawnedShurikenObject = (GameObject)Instantiate(shurikenObject, transform.position, Quaternion.identity), 3);
+        spawnedShurikenObject.GetComponent<ShurikenObject>().attackPower = attackPower;
+    }
+}
---
+++ HanzakiProject/Assets/Scripts/Player/ShurikenObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShurikenObject : MonoBehaviour
+{
+    public float attackPower;
+    public float projectileSpeed;
+
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.Translate(new Vector3(projectileSpeed * Time.deltaTime, 0, 0));
+	}
+}
