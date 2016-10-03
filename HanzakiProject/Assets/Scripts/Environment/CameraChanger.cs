c9647ff8dc5127f892cfaa6d3991f45b505779c4
Sascha <kingosman6@gmail.com>
2016-10-03 16:33:31 +0200

    Working on sliding puzzle

---
+++ HanzakiProject/Assets/Scripts/Environment/CameraChanger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraChanger : MonoBehaviour
+{
+    public bool cameraChanged;
+    public GameObject cameraPos;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if(cameraChanged)
+            {
+                Camera.main.gameObject.GetComponent<CameraController>().inPuzzle = false;
+                cameraChanged = false;
+            }
+            else
+            {
+                Camera.main.gameObject.GetComponent<CameraController>().followObject = cameraPos;
+                Camera.main.gameObject.GetComponent<CameraController>().inPuzzle = true;
+                cameraChanged = true;
+            }
+        }
+    }
+
+
+}
